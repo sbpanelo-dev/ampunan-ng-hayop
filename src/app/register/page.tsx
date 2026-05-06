9b7280b8d526cd6b13ac5bfaa7af0925800bb108
Sherwin <sbpanelo@gbox.ncf.edu.ph>
2026-05-06 20:25:54 +0800

    Update userdashboard and admin dashboard

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -169,9 +169,9 @@ export default function LoginPage() {
 
       setTimeout(() => {
         if (userData.role.toLowerCase().includes('admin')) {
-          router.push('/admindashboard');
-        } else {
           router.push('/userdashboard');
+        } else {
+          router.push('/admindashboard');
         }
       }, 1000);
     } catch {
